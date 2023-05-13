72d9b4a8459ea64b0bd305aba0f7c6dae02653b5
paivabenja <paivabenja05@gmail.com>
2023-05-13 01:32:24 -0300

    backup

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -7,12 +7,12 @@ public class background : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("holis");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = transform.position + new Vector3(1, 0, 0);
     }
 }
--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-  [SerializeField] List <GameObject> obstacleList;
-  [SerializeField] float timeCounter;
+    [SerializeField]
+    List<GameObject> obstacleList;
+
+    [SerializeField]
+    float timeCounter;
+
     // Update is called once per frame
     void Update()
     {
-      timeCounter += Time.deltaTime;
-      if (timeCounter > 1){
-        timeCounter = 0;
-        Instantiate (obstacleList[Random.Range(0,1)]);
-      } 
+        timeCounter += Time.deltaTime;
+        if (timeCounter > 1)
+        {
+            timeCounter = 0;
+            Instantiate(obstacleList[Random.Range(0, 1)]);
+        }
     }
 }
